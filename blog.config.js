24a0f7dc79f037b964d753bfe1076a3192e96932
Ryouissei <ryouissei@outlook.com>
2026-05-31 23:40:24 +0900

    Use Notion API for post listing

--- blog.config.js
+++ blog.config.js
@@ -24,6 +24,8 @@ const BLOG = {
   },
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
   notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
+  notionApiToken: process.env.NOTION_API_TOKEN,
+  notionDataSourceId: process.env.NOTION_DATA_SOURCE_ID,
   analytics: {
     provider: 'ga', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
     ackeeConfig: {
